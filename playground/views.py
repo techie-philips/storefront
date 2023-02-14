4c09f67bf6c87a3d998669f6842d7bcc68d2b879
Philips Jonah <softengineer.philips@gmail.com>
2023-02-14 15:23:09 +0100

    updated .gitignore file.

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import django
 
 # Create your views here.
+
+
+def welcome_page(request):
+    user = 'John Doe'
+    return django.http.HttpResponse(f'Welcome, {user}! this is django')
